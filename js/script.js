5f9c285f841cd9d0f44904de4f44650dfd3f9efb
Ntwanano Rikhotso <sanele.rikhotso.99@gmail.com>
2021-04-25 23:30:35 +0200

    showcase done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var showcase = new Typed(".typed", {
+  strings: ["Computer scientist", "Python Developer", "Solutionist"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000,
+});
