d68f845edef148aa7a7364e2ea9e86313ac5e56d
Ntwanano Rikhotso <sanele.rikhotso.99@gmail.com>
2021-05-07 17:29:08 +0200

    added sidebar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 var showcase = new Typed(".typed", {
-  strings: ["Computer Scientist", "Python Developer", "Solutionist"],
+  strings: ["Computer Scientist", "Python Developer", "Solutionist "],
   loop: true,
   typeSpeed: 100,
   backSpeed: 50,
   backDelay: 2000,
+});
+
+document.querySelector(".nav-toggler").addEventListener("click", () => {
+  document.querySelector(".sidebar").classList.toggle("side-active");
+  document.querySelector(".icons").classList.toggle("toggler");
 });
